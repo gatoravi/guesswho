424c63d4493d061b1d1a0a6a5b88d6e45914f7ef
Avinash Ramu <trackavinash@gmail.com>
2015-11-16 15:30:47 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='guesswho',
+      version='0.1',
+      description='Infer location based on tweets.',
+      url='http://github.com/gatoravi/guesswho',
+      author='Avi Ramu',
+      author_email='avinash3003@yahoo.co.in',
+      license='MIT',
+      packages=['guesswho'],
+      zip_safe=False)
